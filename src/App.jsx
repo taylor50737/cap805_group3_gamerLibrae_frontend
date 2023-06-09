4084a0f95baa9e6955d5dcafd9fe0b27e1c9678d
Taylor Lo <taylorloph@gmail.com>
2023-06-09 15:39:18 -0400

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello 852_code9
+    </h1>
+  )
+}
+
+export default App
---
+++ src/MemberPanel/pages/ChangeInfo.jsx
@@ -0,0 +1,5 @@
+const ChangeInfo = () => {
+    return <h2>This is change info page</h2>
+};
+
+export default ChangeInfo;
