ffe3e9666436ecfa29e05c6c3b8f77a6ccb36742
Taylor Lo <taylorloph@gmail.com>
2023-06-17 14:08:21 -0400

    Added FontAwesome icon library and example code

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,22 @@ const App = () => {
     <>
       <Navbar />
       <Container>
-        <h1 className='text-3xl font-bold underline'>Hello 852_code9</h1>
+        <h1 className='text-3xl font-bold underline'><i className='fa-solid fa-hand' /> Hi 852_code_9</h1>
+        <div>
+          <button>
+            <i className='fa fa-pen' /> Write a Review
+          </button>
+        </div>
+        <div>
+          <button>
+            <i className='fa fa-send' /> Send us a message
+          </button>
+        </div>
+        <div>
+          <button>
+            <i className='fa fa-user' /> Login
+          </button>
+        </div>
         <AdminPanel />
       </Container>
       <Footer />
