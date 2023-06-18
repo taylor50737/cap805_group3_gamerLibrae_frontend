c5472ab692317e8894d8e110a687196cf6b4912a
ls006493 <ls006493@gmail.com>
2023-06-18 01:46:34 -0400

    Fix import error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import ChangeInfo from './MemberPanel/pages/ChangeInfoPage.jsx';
+import ChangeInfo from './MemberPanel/pages/ChangeInfo.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
