ceaf632764d1b637942a06f66f1b760c61957d73
ls006493 <ls006493@gmail.com>
2023-06-21 10:00:04 -0400

    Add route layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
 import { Navbar } from './Components/Navbar';
 import { Footer } from './Components/Footer';
 import { Container } from '@mui/material';
-import AdminPanel from './AdminPanel/AdminPanel';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Container>
-        <h1 className='text-3xl font-bold underline'><i className='fa-solid fa-hand' /> Hi 852_code_9</h1>
-        <div>
-          <button>
-            <i className='fa fa-pen' /> Write a Review
-          </button>
-        </div>
-        <div>
-          <button>
-            <i className='fa fa-send' /> Send us a message
-          </button>
-        </div>
-        <div>
-          <button>
-            <i className='fa fa-user' /> Login
-          </button>
-        </div>
-        <AdminPanel />
+        <Outlet />
       </Container>
       <Footer />
     </>
