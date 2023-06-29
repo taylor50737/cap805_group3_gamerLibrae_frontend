68cb2891b708fad1d9e73146a7e23f2ea7ef2fe1
Taylor Lo <taylorloph@gmail.com>
2023-06-29 16:35:09 -0400

    Added Member change info form

--- src/MemberPanel/components/SideNavigation/MainNavigation.jsx
+++ src/MemberPanel/components/SideNavigation/MainNavigation.jsx
@@ -6,7 +6,6 @@ import NavLinks from './NavLinks';
 const MainNavigation = () => {
   return (
     <div>
-      <CustomBreadcrumbs />
       <aside className='sm:w-full lg:w-64'>
         <div className='h-full overflow-y-auto rounded-lg bg-gray-700 px-3 py-4'>
           <div className='flex flex-col items-center pb-4'>
--- src/MemberPanel/pages/ChangeInfo.jsx
+++ src/MemberPanel/pages/ChangeInfo.jsx
@@ -1,8 +1,39 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
 const ChangeInfo = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [data, setData] = useState('');
+
   return (
-    <>
-      <div className='p-4 pt-2'>This is Change Info page</div>
-    </>
+    <div className='flex flex-col items-center rounded-lg bg-gray-700 pb-4'>
+      <h1 className='text-3xl'>Change Member Info</h1>
+      <div>
+        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+          <input
+            {...register('userName', { required: true, maxLength: 20 })}
+            aria-invalid={errors.userName ? 'true' : 'false'}
+            placeholder='Username'
+          />
+          {errors.userName?.type === 'required' && <p role='alert'>Username is required</p>}
+          {errors.userName?.type === 'maxLength' && (
+            <p role='alert'>Only allow maximum 20 characters</p>
+          )}
+          <input
+            {...register('url', { required: 'URL is required' })}
+            aria-invalid={errors.url ? 'true' : 'false'}
+            placeholder='YouTube/Twitch URL'
+          />
+          {errors.url && <p role='alert'>{errors.url?.message}</p>}
+          <p>{data}</p>
+          <input type='submit' />
+        </form>
+      </div>
+    </div>
   );
 };
 
