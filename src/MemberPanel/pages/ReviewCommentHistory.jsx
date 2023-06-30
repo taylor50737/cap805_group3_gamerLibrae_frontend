d732192e6c585039ca13c7e035289bf46a8a2cb8
Taylor Lo <taylorloph@gmail.com>
2023-06-30 17:32:04 -0400

    Upload Profile Picture HTML

--- src/MemberPanel/pages/ReviewCommentHistory.jsx
+++ src/MemberPanel/pages/ReviewCommentHistory.jsx
@@ -1,36 +1,34 @@
 const ReviewCommentHistory = () => {
   return (
     <>
-      <div>
-        <div className='rounded-lg bg-gray-700'>
-          <div className='p-4'>
-            <a
-              href='#'
-              className='block text-lg font-medium leading-tight text-black hover:underline'
-            >
-              Your Reviews
-            </a>
-            <p className='mt-2'>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla tempore rerum nisi
-              illum iste, dolores animi possimus aliquam ullam unde aut impedit facere autem labore,
-              minus ipsam? Doloribus facere, tempore ducimus, tempora explicabo deserunt qui iusto
-              porro officiis nostrum ad dolorem. Veritatis harum quo ea illo, quae porro ullam
-              aperiam, sit laudantium sint, corporis distinctio. Tempore suscipit nobis, debitis
-              iure culpa voluptate dolorum obcaecati quibusdam cumque soluta quisquam aliquam,
-              accusantium molestiae illum. Eius mollitia dolorem amet aperiam veniam, reiciendis
-              consequatur. Nemo labore beatae praesentium enim similique, dolore delectus deserunt
-              nisi voluptas. Magni distinctio dolore itaque odit error consequuntur quasi iure
-              incidunt nihil hic sint eveniet, ab sit? Ipsa tempora quod ipsam, accusamus odit
-              dolores aliquam illum culpa optio sed? Corrupti ipsa tempore maiores voluptates
-              doloremque molestias quasi, totam vitae. Impedit doloremque quam laboriosam tenetur,
-              ut cupiditate. Suscipit animi quis deleniti itaque eligendi nostrum quisquam saepe at
-              beatae! Aliquam itaque, nesciunt magni officia dolor ducimus, nulla autem eligendi,
-              assumenda doloribus eius consequatur aperiam quas sequi maxime adipisci fugiat tenetur
-              eveniet quasi sapiente dicta? Facilis ipsam dolorum rem eius, nihil natus! Rem
-              laudantium fuga culpa quidem illo aliquid id! Nam minima nesciunt facilis. Ad facilis
-              consequatur ipsam autem cumque similique cum aperiam!
-            </p>
-          </div>
+      <div className='rounded-lg bg-gray-700'>
+        <div className='p-4'>
+          <a
+            href='#'
+            className='block text-lg font-medium leading-tight text-black hover:underline'
+          >
+            Your Reviews
+          </a>
+          <p className='mt-2'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla tempore rerum nisi illum
+            iste, dolores animi possimus aliquam ullam unde aut impedit facere autem labore, minus
+            ipsam? Doloribus facere, tempore ducimus, tempora explicabo deserunt qui iusto porro
+            officiis nostrum ad dolorem. Veritatis harum quo ea illo, quae porro ullam aperiam, sit
+            laudantium sint, corporis distinctio. Tempore suscipit nobis, debitis iure culpa
+            voluptate dolorum obcaecati quibusdam cumque soluta quisquam aliquam, accusantium
+            molestiae illum. Eius mollitia dolorem amet aperiam veniam, reiciendis consequatur. Nemo
+            labore beatae praesentium enim similique, dolore delectus deserunt nisi voluptas. Magni
+            distinctio dolore itaque odit error consequuntur quasi iure incidunt nihil hic sint
+            eveniet, ab sit? Ipsa tempora quod ipsam, accusamus odit dolores aliquam illum culpa
+            optio sed? Corrupti ipsa tempore maiores voluptates doloremque molestias quasi, totam
+            vitae. Impedit doloremque quam laboriosam tenetur, ut cupiditate. Suscipit animi quis
+            deleniti itaque eligendi nostrum quisquam saepe at beatae! Aliquam itaque, nesciunt
+            magni officia dolor ducimus, nulla autem eligendi, assumenda doloribus eius consequatur
+            aperiam quas sequi maxime adipisci fugiat tenetur eveniet quasi sapiente dicta? Facilis
+            ipsam dolorum rem eius, nihil natus! Rem laudantium fuga culpa quidem illo aliquid id!
+            Nam minima nesciunt facilis. Ad facilis consequatur ipsam autem cumque similique cum
+            aperiam!
+          </p>
         </div>
       </div>
       <div className='py-4'>
@@ -40,7 +38,7 @@ const ReviewCommentHistory = () => {
               href='#'
               className='block text-lg font-medium leading-tight text-black hover:underline'
             >
-              Lorem ipsum dolor sit amet.
+              Your Comments
             </a>
             <p className='mt-2'>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum tempore architecto
--- src/MemberPanel/pages/UploadProfilePic.jsx
+++ src/MemberPanel/pages/UploadProfilePic.jsx
@@ -1,10 +1,39 @@
-import MainNavigation from '../components/SideNavigation/MainNavigation';
+import React, { useState } from 'react';
 
 const UploadProfilePic = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
-    <>
-      <div className='p-4 pt-2'>This upload profile pic page</div>
-    </>
+    <div className='flex flex-col items-center rounded-lg bg-gray-700 pb-4'>
+      <h1 className='text-3xl'>Upload Profile Picture</h1>
+      <form>
+        {selectedImage && (
+          <div>
+            <img
+              alt='not found'
+              width={'250px'}
+              src={URL.createObjectURL(selectedImage)}
+              className='h-48 w-48 rounded-full'
+            />
+            <br />
+            <button onClick={() => setSelectedImage(null)}>Remove</button>
+          </div>
+        )}
+
+        <br />
+        <br />
+
+        <input
+          type='file'
+          name='myImage'
+          onChange={(event) => {
+            console.log(event.target.files[0]);
+            setSelectedImage(event.target.files[0]);
+          }}
+        />
+        <input type='submit' />
+      </form>
+    </div>
   );
 };
 
