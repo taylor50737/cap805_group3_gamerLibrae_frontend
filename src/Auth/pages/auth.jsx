b8537546c0bdc8f1d25d4a940865f225a84f3186
Taylor Lo <taylorloph@gmail.com>
2023-07-01 07:25:00 -0400

    GL-82, 83, 84

---
+++ src/Auth/pages/auth.jsx
@@ -0,0 +1,57 @@
+import { useState, useContext } from 'react';
+
+import { CustomUseForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth_context';
+
+const Auth = () => {
+  const auth = useContext(AuthContext);
+  const [isLoginMode, setIsLoginMode] = useState(true);
+
+  const [formState, inputHandler, setFormData] = CustomUseForm(
+    {
+      email: {
+        value: '',
+        isValid: false,
+      },
+      password: {
+        value: '',
+        isValid: false,
+      },
+    },
+    false,
+  );
+
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid,
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: '',
+            isValid: false,
+          },
+        },
+        false,
+      );
+    }
+    setIsLoginMode((prevMode) => !prevMode);
+  };
+
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+    auth.login();
+  };
+
+  return <h1>Test</h1>;
+};
+
+export default Auth;
