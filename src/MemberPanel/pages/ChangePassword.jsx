94d097c5016a57904e1a28ee2dd06996b0b5c0d3
Taylor Lo <taylorloph@gmail.com>
2023-07-03 13:51:02 -0400

    Finished ChangeInfo & ChangePassword CSS

--- src/MemberPanel/pages/ChangePassword.jsx
+++ src/MemberPanel/pages/ChangePassword.jsx
@@ -1,53 +1,75 @@
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { Input } from '@mui/material';
+
+import CustomInput from '../../shared/components/FormElements/CustomInput';
+import CustomButton from '../../shared/components/FormElements/CustomButton';
+import { CustomUseForm } from '../../shared/hooks/form-hook';
+import { VALIDATOR_MINLENGTH, VALIDATOR_CONFIRMPASSWORD } from '../../shared/util/validators';
 
 const ChangePassword = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const [data, setData] = useState('');
+  const [successSubmission, setSuccessSubmission] = useState('');
+  const [formState, inputHandler, setFormData] = CustomUseForm(
+    {
+      currentPassword: {
+        value: '',
+        isValid: false,
+      },
+      newPassword: {
+        value: '',
+        isValid: false,
+      },
+      confirmPassword: {
+        value: '',
+        isValid: false,
+      },
+    },
+    false,
+  );
+  const changePWSubmitHandler = (event) => {
+    event.preventDefault();
+    setSuccessSubmission('You have successfully changed your password!');
+  };
 
   return (
-    <div className='flex flex-col items-center rounded-lg bg-gray-700 pb-4'>
-      <h1 className='text-3xl'>Change Password</h1>
-      <div>
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-          <Input
-            {...register('currentPassword', { required: true, minLength: 8 })}
-            aria-invalid={errors.userName ? 'true' : 'false'}
-            placeholder='Current Password'
-          />
-          {errors.currentPassword?.type === 'required' && (
-            <p role='alert'>Current Password is required</p>
-          )}
-          {errors.currentPassword?.type === 'minLength' && (
-            <p role='alert'>At least 8 characters</p>
-          )}
-          <Input
-            {...register('newPassword', { required: true, minLength: 8 })}
-            aria-invalid={errors.userName ? 'true' : 'false'}
-            placeholder='New Password'
-          />
-          {errors.newPassword?.type === 'required' && <p role='alert'>New Password is required</p>}
-          {errors.newPassword?.type === 'minLength' && <p role='alert'>At least 8 characters</p>}
-          <Input
-            {...register('confirmPassword', { required: true, minLength: 8 })}
-            aria-invalid={errors.userName ? 'true' : 'false'}
-            placeholder='Confirm Password'
-          />
-          {errors.confirmPassword?.type === 'required' && (
-            <p role='alert'>Confirm Password is required</p>
-          )}
-          {errors.confirmPassword?.type === 'minLength' && (
-            <p role='alert'>At least 8 characters</p>
-          )}
-          <p>{data}</p>
-          <input type='submit' />
-        </form>
-      </div>
+    <div className='flex flex-col items-center rounded-lg bg-gray-700 p-4 pb-4'>
+      <h2 className='text-3xl'>Change Password</h2>
+      <form className='flex flex-col items-center' onSubmit={changePWSubmitHandler}>
+        <CustomInput
+          element='input'
+          id='currentPassword'
+          type='password'
+          label='Current Password'
+          placeholder='Enter your current password'
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText='Your current password is incorrect!'
+          onInput={inputHandler}
+        />
+        <CustomInput
+          element='input'
+          id='newPassword'
+          type='password'
+          label='New Password'
+          placeholder='Enter a new password with at least 5 characters'
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText='Please enter a valid new password, at least 5 characters.'
+          onInput={inputHandler}
+        />
+        <CustomInput
+          element='input'
+          id='confirmPassword'
+          type='password'
+          label='Confirm New Password'
+          placeholder='Re-enter your password'
+          validators={[VALIDATOR_CONFIRMPASSWORD(formState.inputs.newPassword.value)]}
+          errorText='Please check whether your password is entered correctly!'
+          onInput={inputHandler}
+        />
+        <p className='py-3'>{successSubmission}</p>
+        <div>
+          <CustomButton type='submit' disabled={!formState.isValid}>
+            SUBMIT
+          </CustomButton>
+        </div>
+      </form>
     </div>
   );
 };
