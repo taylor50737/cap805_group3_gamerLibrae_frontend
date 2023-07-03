f7a35d2edaf2c7246d47a3aa568a33f97e7d2ed8
Taylor Lo <taylorloph@gmail.com>
2023-07-03 12:17:37 -0400

    Finished WishList CSS

--- src/MemberPanel/components/WishList/WishListItem.jsx
+++ src/MemberPanel/components/WishList/WishListItem.jsx
@@ -1,16 +1,73 @@
-import { Card } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Grid, Typography, Box, Paper, Chip } from '@mui/material';
 
-const WishListItem = (props) => {
+import Score from '../../../Components/Score';
+
+const WishListItem = ({ games }) => {
   return (
-    <li>
-      <Card>
-        <div>
-          <img src={props.imageUrl} alt={props.game} />
-          <h2>Game title: {props.game}</h2>
-          <p>Description: {props.description}</p>
-        </div>
-      </Card>
-    </li>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+      {games.map((game, i) => (
+        <Paper
+          key={game.gid}
+          elevation={10}
+          sx={{ bgcolor: '#212121', color: 'white', borderRadius: '14px' }}
+        >
+          <Grid container sx={{ m: 0, width: '100%', height: '100%' }}>
+            {/* Cover image */}
+            <Grid item md={3}>
+              <Link to={`/game/${game.gid}`}>
+                <Paper
+                  elevation={10}
+                  sx={{
+                    width: 160,
+                    height: 200,
+                    borderRadius: '14px',
+                    backgroundImage: `url(${game.imgSrc})`,
+                    backgroundSize: '100% 100%',
+                  }}
+                />
+              </Link>
+            </Grid>
+
+            {/* Information */}
+            <Grid item md={7}>
+              <Box sx={{ ml: 2 }}>
+                <Typography sx={{ fontSize: '24px', fontWeight: 800 }}>{game.title}</Typography>
+                <Typography sx={{ fontSize: '16px' }}>
+                  Platform: {game.platform}
+                  <br />
+                  Genre: {game.genre}
+                  <br />
+                  Release Date: {game.releaseDate}
+                  <br />
+                  Mode: {game.mode}
+                </Typography>
+                {game.tags.map((tag, i) => (
+                  <Chip
+                    key={i}
+                    variant='outlined'
+                    label={tag}
+                    sx={{
+                      mr: '4px',
+                      border: 'none',
+                      bgcolor: '#9747ff',
+                      color: '#ffffff',
+                      '.MuiChip-label': { fontSize: '12px', px: '8px' },
+                      '.MuiChip-deleteIcon': { fontSize: '15px' },
+                    }}
+                  />
+                ))}
+              </Box>
+            </Grid>
+
+            {/* Score */}
+            <Grid item md={2} sx={{ display: 'flex', alignItems: 'center' }}>
+              <Score score={game.score} size={100} />
+            </Grid>
+          </Grid>
+        </Paper>
+      ))}
+    </Box>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
 // import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
@@ -89,4 +90,8 @@ import App from './App';
 //   </React.StrictMode>,
 // );
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
