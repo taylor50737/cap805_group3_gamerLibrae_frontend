189ce8e202e5cb2791e77651f08f28b9a7d1f28b
ls006493 <ls006493@gmail.com>
2023-07-03 23:30:29 -0400

    Fix image path

--- src/ReviewEdit/components/GameInfo.jsx
+++ src/ReviewEdit/components/GameInfo.jsx
@@ -4,7 +4,7 @@ export const GameInfo = () => {
   return (
     <Grid container spacing={{ xs: 2, sm: 4, md: 8 }} sx={{ py: 4 }}>
       <Grid item>
-        <img src='/images/carousel/zelda.jpg' className='h-[120px] w-[120px] rounded' />
+        <img src='/images/GameInfo/zelda_big.jpg' className='h-[120px] w-[120px] rounded' />
       </Grid>
       <Grid item>
         <Typography
