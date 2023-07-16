7157f7e68fdcc9d6af0146da28164dd660416ec5
ls006493 <ls006493@gmail.com>
2023-07-15 20:35:48 -0400

    Finish crop image

--- src/AdminPanel/components/AddGameForm.jsx
+++ src/AdminPanel/components/AddGameForm.jsx
@@ -1,21 +1,35 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useCallback } from 'react';
 
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateField } from '@mui/x-date-pickers/DateField';
-import { Autocomplete, Box, TextField, Checkbox, Chip, Grid, Button, Paper } from '@mui/material';
+import {
+  Autocomplete,
+  Box,
+  TextField,
+  Checkbox,
+  Chip,
+  Button,
+  Paper,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Typography,
+} from '@mui/material';
 
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 import dayjs from 'dayjs';
+import Cropper from 'react-easy-crop';
 
 import CustomButton from '../../shared/components/FormElements/CustomButton';
 import CustomImageUpload from '../../shared/components/FormElements/CustomImageUpload';
 import { CustomUseForm } from '../../shared/hooks/form-hook';
+import { cropImage } from '../../shared/util/cropImage';
 
 const cloud_name = 'dpfvhna2t';
 const api_key = '133115664541957';
@@ -254,15 +268,17 @@ const UploadPic = () => {
 };
 
 const AddGameForm = ({ extraSx }) => {
-  const [gameName, setGameName] = useState('');
+  const [name, setName] = useState('');
   const [developer, setDeveloper] = useState('');
   const [releaseDate, setReleaseDate] = useState(dayjs('2000-01-01'));
   const [genres, setGenres] = useState([]);
   const [platforms, setPlatforms] = useState([]);
   const [playModes, setPlayModes] = useState([]);
+  const [banner, setBanner] = useState(null);
+  const [portrait, setPortrait] = useState(null);
 
   const handleReset = () => {
-    setGameName('');
+    setName('');
     setDeveloper('');
     setReleaseDate(dayjs('2000-01-01'));
     setGenres([]);
@@ -270,20 +286,42 @@ const AddGameForm = ({ extraSx }) => {
     setPlayModes([]);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
+    const signatureResponse = await fetch('http://localhost:8080/api/cloudinary/signature');
+    const signatureJson = await signatureResponse.json();
+
+    const formData = new FormData();
+    formData.append('file', banner);
+    formData.append('api_key', api_key);
+    formData.append('signature', signatureJson.signature);
+    formData.append('timestamp', signatureJson.timestamp);
+
+    const cloudinaryResponse = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloud_name}/auto/upload`,
+      {
+        method: 'POST',
+        body: formData,
+      },
+    );
+    const cloudinaryJson = await cloudinaryResponse.json();
+    console.log(cloudinaryJson);
+
     console.log({
-      gameName: gameName,
+      name: name,
       developer: developer,
       releaseDate: releaseDate.toISOString(),
       genres: genres,
       platforms: platforms,
       playModes: playModes,
+      banner: banner,
+      portrait: portrait,
     });
   };
 
   return (
-    <div className='sm:flex sm:flex-row sm:justify-center'>
-      <UploadPic />
+    <>
+      {/* <UploadPic /> */}
+
       <Box
         sx={{
           ...extraSx,
@@ -300,8 +338,8 @@ const AddGameForm = ({ extraSx }) => {
       >
         <CustomInputField
           categoryName='Game'
-          val={gameName}
-          onChange={(event) => setGameName(event.target.value)}
+          val={name}
+          onChange={(event) => setName(event.target.value)}
         />
         <CustomInputField
           categoryName='Developer'
@@ -322,6 +360,7 @@ const AddGameForm = ({ extraSx }) => {
                 },
               }}
               sx={{
+                width: '100%',
                 '& .MuiOutlinedInput-root': {
                   color: 'white',
                   '& fieldset': {
@@ -359,48 +398,181 @@ const AddGameForm = ({ extraSx }) => {
           searchOption={playModes}
           setSearchOption={setPlayModes}
         />
-
-        <Grid container spacing={'10px'}>
-          <Grid item md={4}>
-            <Button
-              variant='contained'
-              fullWidth
-              onClick={handleReset}
-              sx={{
-                bgcolor: '#D9D9D9',
-                color: '#000000',
-                ':hover': {
-                  bgcolor: '#33353d',
-                  color: 'white',
-                },
-              }}
-            >
-              Reset
-            </Button>
-          </Grid>
-          <Grid item md={8}>
-            <Link to='/admin-panel'>
-              <Button
-                variant='contained'
-                fullWidth
-                onClick={handleSubmit}
-                sx={{
-                  bgcolor: '#D9D9D9',
-                  color: '#000000',
-                  ':hover': {
-                    bgcolor: '#33353d',
-                    color: 'white',
-                  },
-                }}
-              >
-                Submit
-              </Button>
-            </Link>
-          </Grid>
-        </Grid>
       </Box>
-    </div>
+      {/* <Typography>
+        Please use .webp format to reduce image size, there is no validation at the moment. Banner will be in 16:9, Portrait will be in 3:4.
+      </Typography> */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '20px',
+          mb: '20px',
+        }}
+      >
+        <ImageUploaderCropper
+          name='Banner'
+          aspectRatio={16 / 9}
+          croppedImage={banner}
+          setCroppedImage={setBanner}
+        />
+        <ImageUploaderCropper
+          name='Portrait'
+          aspectRatio={3 / 4}
+          croppedImage={portrait}
+          setCroppedImage={setPortrait}
+        />
+
+        <Button
+          variant='contained'
+          onClick={handleSubmit}
+          sx={{
+            width: '60%',
+            bgcolor: '#D9D9D9',
+            color: '#000000',
+            ':hover': {
+              bgcolor: '#33353d',
+              color: 'white',
+            },
+          }}
+        >
+          Submit
+        </Button>
+      </Box>
+    </>
   );
 };
+
+const ImageUploaderCropper = ({ name, aspectRatio, croppedImage, setCroppedImage }) => {
+  const [imageSrc, setImageSrc] = useState(null);
+  const [fileName, setFileName] = useState(null);
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  }, []);
+
+  const onFileChange = async (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      setFileName(file.name);
+      let imageDataUrl = await readFile(file);
+      setImageSrc(imageDataUrl);
+      setDialogOpen(true);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '10px',
+      }}
+    >
+      <Typography sx={{ fontWeight: 800, fontSize: '24px' }}>{name}</Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '20px' }}>
+        {/* Custom button that direct to the input element */}
+        <Button
+          sx={{
+            bgcolor: '#4e5154',
+            color: 'white',
+            '&:hover': { bgcolor: '#1a1919' },
+          }}
+          onClick={() => document.getElementById(name).click()}
+        >
+          Upload
+        </Button>
+        <Typography>{fileName}</Typography>
+        <input
+          id={name}
+          type='file'
+          onChange={onFileChange}
+          accept='image/webp'
+          style={{ display: 'none' }}
+        />
+        <Button
+          onClick={() => {
+            setImageSrc(null);
+            setCroppedImage(null);
+            setFileName('');
+            document.getElementById(name).value = '';
+          }}
+          sx={{ bgcolor: '#4e5154', color: 'white', '&:hover': { bgcolor: '#1a1919' } }}
+        >
+          Reset
+        </Button>
+      </Box>
+
+      {/* Diaglog opens when image upload is detected */}
+      <Dialog open={dialogOpen} maxWidth='md' fullWidth>
+        <DialogTitle>Crop Your Image</DialogTitle>
+        <DialogContent>
+          <Box sx={{ position: 'relative', width: '100%', height: 300, background: '#333' }}>
+            <Cropper
+              image={imageSrc}
+              crop={crop}
+              zoom={zoom}
+              aspect={aspectRatio}
+              onCropChange={setCrop}
+              onCropComplete={onCropComplete}
+              onZoomChange={setZoom}
+            />
+          </Box>
+        </DialogContent>
+
+        <DialogActions>
+          <Button
+            onClick={() => setDialogOpen(false)}
+            sx={{
+              bgcolor: '#4e5154',
+              color: 'white',
+              '&:hover': { bgcolor: '#1a1919' },
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={() =>
+              cropImage(imageSrc, croppedAreaPixels, console.log).then((image) => {
+                setCroppedImage(image);
+                setDialogOpen(false);
+              })
+            }
+            sx={{ bgcolor: '#4e5154', color: 'white', '&:hover': { bgcolor: '#1a1919' } }}
+          >
+            Crop
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* Cropped image preview */}
+      {croppedImage && (
+        <img
+          src={croppedImage}
+          style={{
+            height: 200,
+            width: 200 * aspectRatio,
+          }}
+        />
+      )}
+    </Box>
+  );
+};
+
+function readFile(file) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => resolve(reader.result), false);
+    reader.readAsDataURL(file);
+  });
+}
 
 export default AddGameForm;
