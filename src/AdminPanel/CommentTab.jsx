91d57460d7f390c0a502261597d3b114ee143a60
KennethWKY <kennethwong630@gmail.com>
2023-07-27 21:07:16 -0400

    Connected report to backend

--- src/AdminPanel/CommentTab.jsx
+++ src/AdminPanel/CommentTab.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function CommentTab() {
   const fields = ['', 'Index', 'Comment', 'Creator', '#Reports', 'Status'];
-
+  const [isLoading, setIsloading] = useState(true);
   const [comments, setComments] = useState([]);
   const [selectedComments, setSelectedComments] = useState([]);
   const [successMessage, setSuccessMessage] = useState('');
@@ -12,7 +12,8 @@ export default function CommentTab() {
     try {
       const res = await fetch(`${import.meta.env.VITE_API_PATH}/api/comments/`);
       const data = await res.json();
-      setComments(data.comments);
+      await setComments(data.comments);
+      setIsloading(false);
     } catch (error) {
       console.log(error);
     }
@@ -63,7 +64,7 @@ export default function CommentTab() {
   // Calculate the index of the first and last item to display on the current page
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = comments.slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = isLoading ? [] : comments.slice(indexOfFirstItem, indexOfLastItem);
 
   // Calculate the total number of pages
   const totalPages = Math.ceil(comments.length / itemsPerPage);
@@ -135,38 +136,47 @@ export default function CommentTab() {
           </thead>
           <tbody>
             {/* row */}
-            {currentItems.map((comment, index) => (
-              <tr key={index}>
-                <th>
-                  <input
-                    type='checkbox'
-                    className='checkbox'
-                    checked={selectedComments.includes(comment.id)}
-                    onChange={() => {
-                      if (selectedComments.includes(comment.id)) {
-                        setSelectedComments((prevSelectedComments) =>
-                          prevSelectedComments.filter((id) => id !== comment.id),
-                        );
-                      } else {
-                        setSelectedComments((prevSelectedComments) => [
-                          ...prevSelectedComments,
-                          comment.id,
-                        ]);
-                      }
-                    }}
-                  />
-                </th>
-                <td>{indexOfFirstItem + index + 1}</td>
-                <td>
-                  <Link to={`/profile/${comment.commentUserId}`}>
-                    {comment.comment.split('. ')[0]}...
-                  </Link>
+            {/* row */}
+            {isLoading ? (
+              <tr>
+                <td colSpan={fields.length} className='text-center'>
+                  <span className='loading loading-spinner loading-lg'></span>
                 </td>
-                <td>{comment.creator}</td>
-                <td>{comment.reports?.length || 0}</td>
-                <td>{comment.status}</td>
               </tr>
-            ))}
+            ) : (
+              currentItems.map((comment, index) => (
+                <tr key={index}>
+                  <th>
+                    <input
+                      type='checkbox'
+                      className='checkbox'
+                      checked={selectedComments.includes(comment.id)}
+                      onChange={() => {
+                        if (selectedComments.includes(comment.id)) {
+                          setSelectedComments((prevSelectedComments) =>
+                            prevSelectedComments.filter((id) => id !== comment.id),
+                          );
+                        } else {
+                          setSelectedComments((prevSelectedComments) => [
+                            ...prevSelectedComments,
+                            comment.id,
+                          ]);
+                        }
+                      }}
+                    />
+                  </th>
+                  <td>{indexOfFirstItem + index + 1}</td>
+                  <td>
+                    <Link to={`/profile/${comment.commentUserId}`}>
+                      {comment.comment.split('. ')[0]}...
+                    </Link>
+                  </td>
+                  <td>{comment.creator}</td>
+                  <td>{comment.reports?.length || 0}</td>
+                  <td>{comment.status}</td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       </div>
--- src/AdminPanel/UserTab.jsx
+++ src/AdminPanel/UserTab.jsx
@@ -1,7 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function UserTab({ users }) {
+export default function UserTab() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+
+  const fetchUsers = async () => {
+    try {
+      const res = await fetch(`${import.meta.env.VITE_API_PATH}/api/users/`);
+      const data = await res.json();
+      await setUsers(data.users);
+      setIsloading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   const fields = ['ID', 'Username', 'Email', 'Tier', 'Affilate', 'Status'];
 
   const [currentPage, setCurrentPage] = useState(1);
@@ -10,7 +28,7 @@ export default function UserTab({ users }) {
   // Calculate the index of the first and last item to display on the current page
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = users.slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = isLoading ? [] : users.slice(indexOfFirstItem, indexOfLastItem);
 
   // Calculate the total number of pages
   const totalPages = Math.ceil(users.length / itemsPerPage);
@@ -57,26 +75,34 @@ export default function UserTab({ users }) {
           </thead>
           <tbody>
             {/* row */}
-            {currentItems.map((user, index) => (
-              <tr key={user.userName}>
-                <th>
-                  <label>
-                    <input type='checkbox' className='checkbox' />
-                  </label>
-                </th>
-                <td>{indexOfFirstItem + index + 1}</td>
-                <td className='avatar'>
-                  <div className='w-8'>
-                    <img src={user.avatar} />
-                  </div>
+            {isLoading ? (
+              <tr>
+                <td colSpan={fields.length} className='text-center'>
+                  <span className='loading loading-spinner loading-lg'></span>
                 </td>
-                <Link to={`/profile/${user._id}`}>{user.userName}</Link>
-                <td>{user.email}</td>
-                <td>{user.isAdmin === true ? 'Admin' : 'Member'}</td>
-                <td>{user.joinedAffiliation === true ? 'Yes' : 'No'}</td>
-                <td>{user.status}</td>
               </tr>
-            ))}
+            ) : (
+              currentItems.map((user, index) => (
+                <tr key={user.userName}>
+                  <th>
+                    <label>
+                      <input type='checkbox' className='checkbox' />
+                    </label>
+                  </th>
+                  <td>{indexOfFirstItem + index + 1}</td>
+                  <td className='avatar'>
+                    <div className='w-8'>
+                      <img src={user.avatar} />
+                    </div>
+                  </td>
+                  <Link to={`/profile/${user._id}`}>{user.userName}</Link>
+                  <td>{user.email}</td>
+                  <td>{user.isAdmin === true ? 'Admin' : 'Member'}</td>
+                  <td>{user.joinedAffiliation === true ? 'Yes' : 'No'}</td>
+                  <td>{user.status}</td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       </div>
